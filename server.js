39c693c97150dbd103fbbc40a19b923aa3d9838a
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2024-12-04 12:33:15 +0530

    Completed solution for Virtual Assistant API and Status Code API

--- server.js
+++ server.js
@@ -50,6 +50,66 @@ List of Status Codes to Handle:
 200, 201, 204, 400, 401, 403, 404, 405, 429, 500, 502, 503, 504
 */
 
+app.use(express.json());
+
+app.get('/status-info', (req, res)=>{
+  let {code} = req.query;
+  code = parseInt(code);
+  let message = "";
+
+  switch(code){
+    case 200:
+      message= "OK: The request has succeeded. The meaning of this status depends on the HTTP method used.";
+      break;
+    case 201:
+      message= "Created: A resource has been successfully created.";
+      break;
+    case 204:
+      message= "No Content: Request processed successfully, no content returned.";
+      break;
+    case 400:
+      message= "Bad Request: The request is invalid due to client-side errors.";
+      break;
+    case 401:
+      message= "Unauthorized: Authentication is required to access the resource.";
+      break;
+    case 403:
+      message= "Forbidden: Server refuses to authorize the request.";
+      break;
+    case 404:
+      message= "Not Found: The server has not found anything matching the request URI. This is often caused by a missing page or resource.";
+      break;
+    case 405:
+      message= "Method Not Allowed: HTTP method not supported for this resource.";
+      break;
+    case 429:
+      message= "Too Many Requests: User has exceeded rate limits.";
+      break;
+    case 500:
+      message= "Internal Server Error: The server encountered an unexpected condition that prevented it from fulfilling the request.";
+      break;
+    case 502:
+      message= "Bad Gateway: The server received an invalid response from the upstream server.";
+      break;
+    case 503:
+      message= "Service Unavailable: Server temporarily overloaded or under maintenance.";
+      break;
+    case 504:
+      message= "Gateway Timeout: The server did not receive a timely response from the upstream server.";
+      break;
+    default:
+      message= "invalid status code";
+      break;
+  }
+  
+  return res.status(code).json({
+    "status": code,
+    "message": message
+  });
+  
+})
+
+
 const PORT = 3000;
 app.listen(PORT, () => {
     console.log(`Status Code API is running on http://localhost:${PORT}`);
